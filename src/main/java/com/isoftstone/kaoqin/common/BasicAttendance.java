e738c859372bf580fc270bd4088054bf5b172030
wb-chenchaobin <wb-chenchaobin@alibaba-inc.com>
2015-09-11 19:07:23 +0800

    test

--- src/main/java/com/isoftstone/kaoqin/common/BasicAttendance.java
+++ src/main/java/com/isoftstone/kaoqin/common/BasicAttendance.java
@@ -5,7 +5,8 @@ import com.isoftstone.kaoqin.bean.User;
 /**
  * Created by wb-chenchaobin on 2015/9/8.
  */
-public class BasicAttendance {
+
+public class BasicAttendance<T> {
 
     private String msg;
     private int code;//0:不存在；1；存在用户
